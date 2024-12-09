5b95eb9efe0afd518793c30d10fdf0c400436a8c
melinox58 <melinox89@yahoo.fr>
2024-12-09 22:04:02 +0100

    add main css

--- front/src/App.vue
+++ front/src/App.vue
@@ -3,23 +3,9 @@
 </template>
 
 <script>
-import BaseLayout from './components/BaseLayout.vue'
 
-export default {
-  name: 'App',
-  components: {
-    BaseLayout
-  }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
