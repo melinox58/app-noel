d3bb7570ab2a3ddb4b84003fdb917bf94e5578d9
melinox58 <melinox89@yahoo.fr>
2024-12-17 10:33:24 +0100

    add back resolve conflicts ok

--- back/index.js
+++ back/index.js
@@ -1,57 +1,77 @@
-const express = require('express'); //déclare une variable qui importe express
-const port = process.env.PORT || 5000; //déclare une variable pour le port
+const express = require('express'); // Déclare une variable qui importe express
+const PORT = process.env.PORT || 5000;
+const db = require('./config/db-config');
+const mongoose = require('mongoose');
+const dbMongo = require('./config/mongoDb');
 const dotenv = require('dotenv');
+const morgan = require('morgan'); // pour debug requetes
+
 // Charger les variables d'environnement depuis .env.local si disponible, sinon depuis .env
 dotenv.config({ path: '.env.local' });
-const mongoose = require('mongoose');
-const app = express(); //fonction native qui démarre le server
-//middleware qui analyse (parse) le body des requetes et renvoi un objet req.body
-const bodyParser = require('body-parser'); //middleware qui analyse
-// cross origin
-const cors = require('cors'); //autoriser les requetes depuis l'exterieur
+dotenv.config({ path: '.env' });
 
-//base de données mysql
- const mysql = require('mysql2');
+const app = express(); // Fonction native qui démarre le serveur
+
+// Middleware qui analyse (parse) le body des requêtes et renvoie un objet req.body
+const bodyParser = require('body-parser'); // Middleware qui analyse
+app.use(bodyParser.json());
+
+// Cross Origin
+const cors = require('cors'); // Autoriser les requêtes depuis l'extérieur
+
+// Base de données MySQL
+const mysql = require('mysql2');
+db.connect();
+
+//Connexion Mongo (commentée pour le moment)
+(async () => {
+    await dbMongo();
+})();
 
 const corsOptions = {
     origin: 'http://localhost:8080', // Autoriser les requêtes depuis cette origine
-    origin: 'http://192.168.1.60:8080',
     methods: 'GET,POST,PUT,DELETE', // Autoriser les méthodes GET, POST, PUT, DELETE
     allowedHeaders: 'Content-Type, Authorization', // Autoriser les en-têtes Content-Type et Authorization
     credentials: true // Autoriser les cookies et les en-têtes d'autorisation
 };
+
 app.use(cors(corsOptions)); // Utiliser le middleware CORS avec les options configurées
 
-//test
-app.get('/', (req, res)=>{
-    res.json("Test fonctionne")
-})
-//Route Calendar et Cases
-const calendarRoutes = require('./Routes/calendarRoutes');
+// Middleware pour logger les requêtes HTTP
+app.use(morgan('dev'));
+
+// Test
+app.get('/', (req, res) => {
+    res.json("Test fonctionne");
+});
+
+// Route Calendrier et Cases
+const calendarRoutes = require('./routes/calendarRoutes');
 app.use('/api/calendar', calendarRoutes);
+
 const caseRoutes = require('./routes/caseRoutes');
 app.use('/api', caseRoutes);
 
-//bodyParser pour analyser le coprs de la requete
-app.use(bodyParser.json());
 
+// Charger les modèles
+const { Calendar } = require('./models/mysql/calendarModel');
+const Case = require('./models/mysql/caseModel');
 
-//Route users
-const userRoutes = require('./Routes/userRoutes');
-app.use('/api/users', userRoutes); // Ajouter les routes pour les utilisateurs
+// Définir les associations entre Calendar et cases
+// Case.belongsTo(Calendar, { foreignKey: 'calendars_id' });
+// Calendar.hasMany(Case, { foreignKey: 'calendars_id' });
 
+//Route surprise et avis (commentées pour le moment)
+const surpriseRoutes = require('./routes/surpriseRoutes');
+app.use('/api/surprises', surpriseRoutes);
 
-//route avis
-const avisRoutes = require('./Routes/avisRoutes');
+const avisRoutes = require('./routes/avisRoutes');
 app.use('/api/avis', avisRoutes);
 
+// Route users
+const userRoutes = require('./routes/userRoutes');
+app.use('/api/users', userRoutes); // Ajouter les routes pour les utilisateurs
 
-
-
-//route registration
-
-
-
-//route dashbord user
-
-app.listen(port, () => {console.log(`Server started `);})
+app.listen(PORT, () => {
+    console.log(`Serveur en cours d'exécution sur le port ${PORT}`);
+});
