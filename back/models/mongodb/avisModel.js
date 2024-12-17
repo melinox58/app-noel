39596a3e62da38eab95e9886b6051ea058922cd7
nicov51 <nicolas.viennot@laposte.net>
2024-12-17 22:31:56 +0100

    add validations/contraintes on avisModel

--- back/models/mongodb/avisModel.js
+++ back/models/mongodb/avisModel.js
@@ -3,21 +3,39 @@ const mongoose = require('mongoose');
 const AvisSchema = new mongoose.Schema({
     user_id: {
         type: Number,
-        required: true
+        required: [true, 'L\'identifiant utilisateur est requis'],
+        validate: {
+            validator: Number.isInteger,
+            message: 'L\'identifiant utilisateur doit être un nombre entier'
+        }
     },
     calendar_id: {
         type: Number,
-        required: true
+        required: [true, 'L\'identifiant du calendrier est requis'],
+        validate: {
+            validator: Number.isInteger,
+            message: 'L\'identifiant du calendrier doit être un nombre entier'
+        }
     },
     comment: {
         type: String,
-        required: true
+        required: [true, 'Le commentaire est requis'],
+        validate: {
+            validator: function(v) {
+                return v.length >= 2 && v.length <= 500;
+            },
+            message: 'Le commentaire doit avoir entre 2 et 500 caractères'
+        }
     },
     note: {
         type: Number,
-        required: true,
-        min: 0,
-        max: 5
+        required: [true, 'La note est requise'],
+        min: [0, 'La note doit être au moins 0'],
+        max: [5, 'La note doit être au plus 5'],
+        validate: {
+            validator: Number.isInteger,
+            message: 'La note doit être un nombre entier'
+        }
     },
     created_at: {
         type: Date,
