a89351fa13a18c30a0a1a24139977e56b2e61751
nicov51 <nicolas.viennot@laposte.net>
2024-12-17 19:32:13 +0100

    add validations/contraintes on userModel

--- back/models/mysql/userModel.js
+++ back/models/mysql/userModel.js
@@ -2,6 +2,7 @@ const db = require('../../config/db-config.js');
 const { Sequelize, DataTypes } = require('sequelize');
 const bcrypt = require('bcryptjs');
 require('dotenv').config({ path: '../../.env' });
+
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'mysql',
@@ -18,23 +19,52 @@ const User = sequelize.define('User', {
     },
     name: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                msg: 'Le nom est requis'
+            },
+            len: [2, 50] // Le nom doit avoir entre 2 et 50 caractères
+        }
     },
     firstname: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                msg: 'Le prénom est requis'
+            },
+            len: [2, 50] // Le prénom doit avoir entre 2 et 50 caractères
+        }
     },
     email: {
         type: DataTypes.STRING,
         allowNull: false,
-        unique: true
+        unique: true,
+        validate: {
+            isEmail: {
+                msg: 'L\'email doit être une adresse email valide'
+            },
+            notEmpty: {
+                msg: 'L\'email est requis'
+            }
+        }
     },
     password: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                msg: 'Le mot de passe est requis'
+            },
+            len: [6, 100] // Le mot de passe doit avoir entre 6 et 100 caractères
+        }
+    },
+    is_blocked: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
     }
-
-
 });
 
 // Hook pour hacher le mot de passe avant de créer un nouvel utilisateur
