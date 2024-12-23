f3067fa03eebf5286c3d765e5960a7dfe0274de8
melinox58 <melinox89@yahoo.fr>
2024-12-23 09:56:13 +0100

    Fix package.json for Heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: cd back && npm install && npm run start & cd front && npm install && npm run build && npm start
+web: cd back && npm install && npm run start & cd front && npm install && npm run build && http-server front/dist
--- back/index.js
+++ back/index.js
@@ -21,12 +21,12 @@ const { Calendar } = require('./models/mysql/calendarModel');
 // });
 
 // Middleware pour servir les fichiers statiques
-app.use(express.static(path.join(__dirname, 'front/dist')));
+app.use(express.static(path.join(__dirname, '../front/dist')));
 
 // Renvoyer index.html pour toutes les autres requêtes
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'front/dist', 'index.html'));
-});
+    res.sendFile(path.join(__dirname, '../front/dist/index.html'));
+  });
 
 // Charger les variables d'environnement
 if (process.env.NODE_ENV !== 'production') {
