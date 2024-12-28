9e6bdddf1d50489910d29b3b3cf7fdf72101e073
melinox58 <melinox89@yahoo.fr>
2024-12-28 20:16:22 +0100

    debug cors

--- front/src/main.js
+++ front/src/main.js
@@ -5,7 +5,7 @@ import router from './router';
 import axios from 'axios';
 
 // Configuration globale d'Axios
-axios.defaults.baseURL = '/api'; // URL de votre backend
+// axios.defaults.baseURL = '/api'; // URL de votre backend
 // Créez l'application et enregistrez-la dans une variable
 const app = createApp(App);
 
