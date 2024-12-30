04f5d1a790406b0d8a4969c7caea932cbb1b2a4f
melinox58 <melinox89@yahoo.fr>
2024-12-30 13:15:12 +0100

    debug front

--- front/src/components/CalendarComponent.vue
+++ front/src/components/CalendarComponent.vue
@@ -112,12 +112,13 @@ const images = [
 
 <style scoped>
 
-section[data-v-67104fdf] {
+section{
   width: 80vw;
   height: 60vh;
+  margin-right: 10%;
 }
 
-h3[data-v-67104fdf]{
+h3{
   color: blue;
   font-size: 0.8rem;
   margin-top: 2%;
@@ -127,9 +128,9 @@ h2{
   font-size: 0.5rem;
 }
 
-div[data-v-67104fdf] {
+div{
   font-size: 0.4rem;
-  margin-bottom: -6%;
+  margin-bottom: -8%;
 }
 
 input{
@@ -158,11 +159,6 @@ form{
   display: flex;
   flex-direction: column;
   align-items: center;
-}
-
-.content-container > main:nth-child(2) > section:nth-child(4) > aside:nth-child(3) {
-  margin-top: 21%;
-  display: flex;
 }
 
 .btn{
@@ -205,25 +201,25 @@ ul{
   width: 26vw;
 }
 
-.image-table[data-v-67104fdf]{
+.image-table {
   margin-top: 10%;
   width: 100%;                /* La table occupe toute la largeur disponible */
   height: 45vh;               /* Hauteur fixe pour la table */
   overflow: hidden;           /* Empêche tout débordement visible de la table elle-même */
-  scroll-behavior: smooth;    /* Active le défilement fluide */
 }
 
 .image-row {
   display: flex;
   flex-wrap: wrap;            /* Les images passeront à la ligne suivante si l'espace est insuffisant */
   width: 100%;                /* Garantit que l'image-row occupe toute la largeur disponible */
+  overflow-x: auto;           /* Permet un défilement horizontal si nécessaire */
   overflow-y: auto;           /* Permet un défilement vertical si nécessaire */
   height: 100%;               /* Occupe toute la hauteur de la table */
   gap: 2%;
   justify-content: center;    /* Centre les images horizontalement */
 }
 
-.image-container[data-v-67104fdf]{
+.image-container {
   flex: 0 0 10%;              /* Chaque image occupe environ 48% de la largeur (2 par ligne) */
   max-width: 100%;            /* Evite que les images débordent de leur conteneur */
   justify-content: center;    /* Centre l'image horizontalement */
@@ -232,24 +228,23 @@ ul{
 
 @media only screen and (min-width: 768px){
 
-  section[data-v-67104fdf]{
+  main{
+    height: 82vh;
+  }
+
+  section{
     background-color: rgba(255, 255, 255, 0.854);
     width: 80vw;
-    height: 72vh;
+    height: 100vh;
     margin-right: 10%;
     margin-bottom: -8%;
   }
 
-  h3[data-v-67104fdf]{
+  h3{
     font-size: 1.5rem;
     margin-bottom: 2%;
     margin-top: -4.5%;
   }
-
-  .date {
-  margin-left: -6%;
-}
-
 
   p{
     margin-bottom: 0;
@@ -259,9 +254,8 @@ ul{
   font-size: 3rem;
   }
 
-  div[data-v-67104fdf]{
+  div{
     margin-bottom: 0.5%;
-    width: 75vw;
   }
 
   section h2 {
@@ -310,9 +304,7 @@ ul{
   }
 
   .image-row img {
-    width: 20vw;
-    transition: transform 0.3s ease, box-shadow 0.3s ease;
-    cursor: pointer;
+    width: 22vw;
   }
 
   .image-table[data-v-67104fdf] {
@@ -330,6 +322,11 @@ ul{
     justify-content: center;
     align-items: center;
   }
+
+  img {
+    transition: transform 0.3s ease, box-shadow 0.3s ease;
+    cursor: pointer;
+}
 
   img:hover {
     transform: scale(1.1); /* Agrandissement de l'image */
